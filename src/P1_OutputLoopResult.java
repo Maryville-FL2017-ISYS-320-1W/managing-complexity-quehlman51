ae9529524e25ccefc3d03af1e0ad6cbf66dcdb0f
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2017-12-12 01:07:32 -0600

    New changes

--- src/P1_OutputLoopResult.java
+++ src/P1_OutputLoopResult.java
@@ -7,8 +7,9 @@
 
 
 public class P1_OutputLoopResult {
-	
 	static int finalResult;
+	static int printSumOfSquaresTo5;
+	
 
 	public static void main(String[] args) {
 		printSumOfSquaresTo5();
@@ -18,10 +19,11 @@ public class P1_OutputLoopResult {
 		for( int i = 1; i <= 5; i++ ) {
 			int finalResult = 0;
 			finalResult = finalResult + i * i;
-			System.out.println("Final result is: " + finalResult);
 			
+				
 		}
 		
+		System.out.println("Final result is: " + finalResult.printSumOfSquaresTo5);
 		
 	}
 
